90ef83421858cb406c45b21af584e218a3ca3c75
Hr0mE <95316942+Hr0mE@users.noreply.github.com>
2025-05-08 18:21:42 +1000

    Добавлены модели
    Реализована абстракция для конфига и для моделей. Также есть реализация Mistral, как для ЛЛМ (large-latest), так и для эмбедера (mistral-embed)

---
+++ models/config/base.py
@@ -0,0 +1,29 @@
+#models/config/base.py
+from abc import ABC
+from pydantic_settings import BaseSettings
+from pydantic import Field
+from typing import Optional
+
+class BaseModelConfig(BaseSettings, ABC):
+    """Абстрактная базовая конфигурация"""
+    model_name: str
+    temperature: float = 0.7
+    max_tokens: int = 1000
+    timeout: int = Field(30, description="Request timeout in seconds")
+    safe_mode: bool = Field(False, description="Enable safe mode")
+    random_seed: int | None = Field(None, description="Random seed")
+    
+    class Config:
+        env_prefix = "LLM_"
+        case_sensitive = False
+
+class BaseAPIConfig(BaseModelConfig):
+    """Базовый конфиг для API моделей"""
+    api_key: Optional[str] = None
+    api_key_env: Optional[str] = None
+    api_base: str = Field(description="Base API URL")
+
+class BaseLocalConfig(BaseModelConfig):
+    """Базовый конфиг для локальных моделей"""
+    base_url: str
+    model_path: Optional[str] = None
---
+++ models/mistralai.py
@@ -0,0 +1,45 @@
+# models/mistralai.py
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
+from .base import BaseAPIModel
+from .config.models import MistralLargeAPIConfig, MistralEmbedAPIConfig
+
+class MistralLargeModel(BaseAPIModel):    
+    def __init__(self, config: MistralLargeAPIConfig):
+        super().__init__(config)
+        self.config = config
+    
+    def _initialize(self):
+        self.model = ChatMistralAI(
+            model=self.config.model_name,
+            temperature=self.config.temperature,
+            max_tokens=self.config.max_tokens,
+            mistral_api_key=self.config.api_key,
+        )
+    
+    def invoke(self, prompt: str) -> str:
+        response = self.model.invoke(prompt)
+        return response.content
+
+    def stream(self, prompt: str):
+        for chunk in self.model.stream(prompt):
+            yield chunk.content
+
+
+class MistralEmbedModel(BaseAPIModel):    
+    def __init__(self, config: MistralEmbedAPIConfig):
+        super().__init__(config)
+        self.config = config
+    
+    def _initialize(self):
+        self.model = MistralAIEmbeddings(
+            api_key=self.config.api_key, 
+            wait_time=1
+        )
+    
+    def invoke(self, prompt: str) -> str:
+        response = self.model.invoke(prompt)
+        return response.content
+
+    def stream(self, prompt: str):
+        for chunk in self.model.stream(prompt):
+            yield chunk.content
