0dbf24524717220e18646b87f799b077e45d4b5a
Dmitry Lyapin <Dimma046@list.ru>
2025-05-13 01:43:45 +1000

    add: новая цепочка для поисковой части агента

--- agents/chains/__init__.py
+++ agents/chains/__init__.py
@@ -1,3 +1,10 @@
 from .full_chain import FullExecutionChain
+from .faq_chain import FAQExecutionChain
+from .only_search_chain import OnlySearchChain
 
-__all__ = ["FullExecutionChain"]
+__all__ = [
+    "FullExecutionChain", 
+    "FAQExecutionChain", 
+    "OnlySearchChain"
+
+]
---
+++ agents/chains/only_search_chain.py
@@ -0,0 +1,40 @@
+from .base import BaseChain
+from typing import List
+from agents import nodes
+
+class OnlySearchChain(BaseChain):
+    """Полная цепочка выполнения с основными нодами"""
+    
+    def __init__(self):
+        super().__init__()
+        
+        node_list: List[nodes.BaseNode] = [
+            nodes.RagNode,
+            nodes.CritiqueNode,
+            nodes.FinalizeNode,
+            nodes.FirstStepNode,
+            nodes.ReasonNode,
+            nodes.SearchNode,
+            nodes.WriteNode
+        ]
+
+        #TODO попробовать по-максимуму убрать goto и перенести всё сюда
+        edge_list = [
+            # (nodes...., nodes.MannerExtractNode),  #  TODO: нода декомпозиции(по картинке то)
+            (nodes.ReasonNode, nodes.FirstStepNode),
+            (nodes.WriteNode, nodes.CritiqueNode),
+            (nodes.SearchNode, nodes.WriteNode),
+            (nodes.RagNode, nodes.WriteNode),
+        ]
+
+        # Регистрация нод
+        for node in node_list:
+            self.add_node(name=node().get_name(), node=node().execute)
+        
+        # Определение связей
+        for (source_edge, target_edge) in edge_list:
+            self.add_edge(source_edge().get_name(), target_edge().get_name())
+     
+        # Настройка точек входа/выхода
+        self.set_entry_point(nodes.ReasonNode().get_name())
+        self.set_exit_point(nodes.FinalizeNode().get_name())
