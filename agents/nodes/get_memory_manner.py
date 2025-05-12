bf8d0684bbeb7e9992fbb93a739764646a9cc515
Dmitry Lyapin <Dimma046@list.ru>
2025-05-13 01:34:29 +1000

    Настройка gitignore

--- agents/nodes/get_memory_manner.py
+++ agents/nodes/get_memory_manner.py
@@ -18,15 +18,15 @@ class GetMannerFromMemoryNode(BaseNode):
     namespace = ("user_info", user_uuid)
     results = memory_store.search(namespace)
 
-    # memory_item = results[-1].dict() if results else None
-    # manner_item = memory_item["value"].get("manner", None) if memory_item else None
-    # manner = MannerInfo(**manner_item) if manner_item else None
+    memory_item = results[-1].dict() if results else None
+    manner_item = memory_item["value"].get("manner", None) if memory_item else None
+    manner = MannerInfo(**manner_item) if manner_item else None
 
-    # if manner:
-    #   return Command(
-    #     update={
-    #       "manner": manner
-    #     }
-    #   )
+    if manner:
+      return Command(
+        update={
+          "manner": manner
+        }
+      )
     
     return Command()
