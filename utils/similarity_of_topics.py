8a2bcf5fccb1882912c395abecc540f2344cc942
FREDY129053 <fredy1290534518@gmail.com>
2025-06-07 00:52:05 +1000

    upd: семантических поиск схожих тем

--- utils/similarity_of_topics.py
+++ utils/similarity_of_topics.py
@@ -1,13 +1,25 @@
-import difflib
+from models import NomicEmbedModel
+from models.config import NomicEmbedAPIConfig as model_config
+import numpy as np
+from typing import List, Optional
 
-from typing import List, Union
+model = NomicEmbedModel(config=model_config())
 
 
-def find_similar_theme(theme: str, themes: List[str], threshold=0.9) -> Union[str, None]:
-    """Вычисление схожести тем"""
+def find_similar_theme(theme: str, themes: List[str], threshold=0.85) -> Optional[str]:
+    """Поиск схожей темы по смыслу среди существующих тем
+
+    Args:
+        theme (str): тема, которой надо найти схожую
+        themes (List[str]): все существующие темы
+        threshold (float, optional): порог сходства(от 0 до 1). Defaults to 0.85.
+
+    Returns:
+        Optional[str]: найденная тема или None, если такой нет
+    """
     for existing_theme in themes:
-        similarity = difflib.SequenceMatcher(None, theme.lower(), existing_theme.lower()).ratio()
-        if similarity >= threshold:
+        emb1, emb2 = model.embed_query(theme), model.embed_query(existing_theme)
+        if np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2)) >= threshold:
             return existing_theme
 
     return None
